f1c9c4e2810b1b43e2d7158275abe4c24e8ba6b6
Morris Kyn <morris.kyn@gmail.com>
2021-07-08 08:10:10 -0400

    Home page grid

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Game from "./Pages/bugGame";
 import "./index.css";
 import Help from "./Pages/bugGameHelp";
-import { Login, Home } from "./Pages/login";
+import {Login} from "./Pages/login";
+import Home from "./Pages/home";
 
 const App = () => {
   return (
