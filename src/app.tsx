413eb7b4fc7a557ca795625a6a6c6c4f65fba13a
Htoomyat Naing <2myat9@gmail.com>
2021-07-09 11:33:26 -0700

    Resync with Mo's thumbnail + Add treeJournal

--- src/app.tsx
+++ src/app.tsx
@@ -3,12 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Game from "./Pages/bugGame";
 import "./index.css";
 import Help from "./Pages/bugGameHelp";
-<<<<<<< HEAD
 import { Login } from "./Pages/login";
 import Home from "./Pages/home";
-=======
-import { Login, Home } from "./Pages/login";
->>>>>>> 2d96690858295b015c10c5517c3cfd2508d71e9d
 import TreeJournal from "./Pages/treeJournal";
 
 const App = () => {
