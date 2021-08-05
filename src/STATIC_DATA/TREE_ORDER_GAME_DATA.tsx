8c3deb60425bd5e856d44af79a114630498f64f7
Htoomyat Naing <2myat9@gmail.com>
2021-08-05 11:29:39 -0700

    Add tree planting order game

---
+++ src/STATIC_DATA/TREE_ORDER_GAME_DATA.tsx
@@ -0,0 +1,32 @@
+export const TREE_ORDER_GAME_DATA = {
+  STEP1: {
+    id: "STEP1",
+    desc: "Find an appropriate planting site",
+    imgUrl:
+      "https://us.123rf.com/450wm/dollygrey/dollygrey1808/dollygrey180800010/107783765-watering-can.jpg?ver=6",
+  },
+  STEP2: {
+    id: "STEP2",
+    desc: "Dig a hole in the ground",
+    imgUrl:
+      "https://us.123rf.com/450wm/dollygrey/dollygrey1808/dollygrey180800010/107783765-watering-can.jpg?ver=6",
+  },
+  STEP3: {
+    id: "STEP3",
+    desc: "Place a seed in the hole",
+    imgUrl:
+      "https://us.123rf.com/450wm/dollygrey/dollygrey1808/dollygrey180800010/107783765-watering-can.jpg?ver=6",
+  },
+  STEP4: {
+    id: "STEP4",
+    desc: "Water the soil",
+    imgUrl:
+      "https://us.123rf.com/450wm/dollygrey/dollygrey1808/dollygrey180800010/107783765-watering-can.jpg?ver=6",
+  },
+  STEP5: {
+    id: "STEP5",
+    desc: "Wash your hands",
+    imgUrl:
+      "https://us.123rf.com/450wm/dollygrey/dollygrey1808/dollygrey180800010/107783765-watering-can.jpg?ver=6",
+  },
+};
