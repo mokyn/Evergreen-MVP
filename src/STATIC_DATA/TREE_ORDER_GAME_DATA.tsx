324d5fa891e25de724f2b6e8f5a2affd55e3ba89
Htoomyat Naing <2myat9@gmail.com>
2021-08-11 14:19:00 -0700

    Change tree planting game data

--- src/STATIC_DATA/TREE_ORDER_GAME_DATA.tsx
+++ src/STATIC_DATA/TREE_ORDER_GAME_DATA.tsx
@@ -1,38 +1,39 @@
+import treeplanting1 from "../images/treeplanting/treeplanting1.png";
+import treeplanting2 from "../images/treeplanting/treeplanting2.jpg";
+import treeplanting3 from "../images/treeplanting/treeplanting3.png";
+import treeplanting4 from "../images/treeplanting/treeplanting4.png";
+import treeplanting5 from "../images/treeplanting/treeplanting5.jpeg";
+import treeplanting6 from "../images/treeplanting/treeplanting6.jpg";
+
 export const TREE_ORDER_GAME_DATA = {
   STEP1: {
     id: "STEP1",
-    desc: "Find an appropriate planting site",
-    imgUrl:
-      "https://us.123rf.com/450wm/dollygrey/dollygrey1808/dollygrey180800010/107783765-watering-can.jpg?ver=6",
+    desc: "Choose the right tree for the right place",
+    imgUrl: treeplanting1,
   },
   STEP2: {
     id: "STEP2",
-    desc: "Dig a hole in the ground",
-    imgUrl:
-      "https://us.123rf.com/450wm/dollygrey/dollygrey1808/dollygrey180800010/107783765-watering-can.jpg?ver=6",
+    desc: "Dig a hole, as deep as the root ball and twice the width",
+    imgUrl: treeplanting2,
   },
   STEP3: {
     id: "STEP3",
-    desc: "Place the tree in the hole",
-    imgUrl:
-      "https://us.123rf.com/450wm/dollygrey/dollygrey1808/dollygrey180800010/107783765-watering-can.jpg?ver=6",
+    desc: "Put the tree in the hole. Make sure the root collar is 1 inch above the natural ground",
+    imgUrl: treeplanting3,
   },
   STEP4: {
     id: "STEP4",
     desc: "Build a soil berm around the tree",
-    imgUrl:
-      "https://us.123rf.com/450wm/dollygrey/dollygrey1808/dollygrey180800010/107783765-watering-can.jpg?ver=6",
+    imgUrl: treeplanting4,
   },
   STEP5: {
     id: "STEP5",
     desc: "Stake the tree",
-    imgUrl:
-      "https://us.123rf.com/450wm/dollygrey/dollygrey1808/dollygrey180800010/107783765-watering-can.jpg?ver=6",
+    imgUrl: treeplanting5,
   },
   STEP6: {
     id: "STEP6",
-    desc: "Water the tree and add mulch",
-    imgUrl:
-      "https://us.123rf.com/450wm/dollygrey/dollygrey1808/dollygrey180800010/107783765-watering-can.jpg?ver=6",
+    desc: "Water slowly and deeply and add mulch in a donut shape (don't touch the trunk!)",
+    imgUrl: treeplanting6,
   },
 };
